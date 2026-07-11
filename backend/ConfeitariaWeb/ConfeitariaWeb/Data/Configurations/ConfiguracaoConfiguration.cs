76d2b89480e448e8363ca69ea243442c78c93528
LucianoR8 <luciribsantos@gmail.com>
2026-07-10 23:04:24 -0300

    Adicionando ConfiguracaoConfiguration e ProdutoConfiguration para separação de responsabilidades no AppDbContext, além de adicionar as especificações dos endpoints do sistema.

--- backend/ConfeitariaWeb/ConfeitariaWeb/Data/Configurations/ConfiguracaoConfiguration.cs
+++ backend/ConfeitariaWeb/ConfeitariaWeb/Data/Configurations/ConfiguracaoConfiguration.cs
@@ -1,6 +1,64 @@
-﻿namespace ConfeitariaWeb.Data.Configurations
+﻿using ConfeitariaWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConfeitariaWeb.Data.Configurations
 {
-    public class ConfiguracaoConfiguration
+    public class ConfiguracaoConfiguration : IEntityTypeConfiguration<Configuracao>
     {
+        public void Configure(EntityTypeBuilder<Configuracao> builder)
+        {
+            builder.ToTable("configuracoes");
+
+            builder.HasKey(config => config.IdConfiguracao);
+
+            builder.Property(config => config.IdConfiguracao)
+                   .HasColumnName("id_configuracao");
+
+            builder.Property(config => config.NomeLoja)
+                   .HasColumnName("nome_loja");
+
+            builder.Property(config => config.Instagram)
+                   .HasColumnName("instagram");
+
+            builder.Property(config => config.Endereco)
+                   .HasColumnName("endereco");
+
+            builder.Property(config => config.WhatsApp)
+                   .HasColumnName("whatsapp");
+
+            builder.Property(config => config.Facebook)
+                   .HasColumnName("facebook");
+
+            builder.Property(config => config.Telefone)
+                   .HasColumnName("telefone");
+
+            builder.Property(config => config.AbreAs)
+                   .HasColumnName("abre_as");
+
+            builder.Property(config => config.FechaAs)
+                   .HasColumnName("fecha_as");
+
+            builder.Property(config => config.Email)
+                   .HasColumnName("email");
+
+            builder.Property(config => config.QuantidadeMaximaDestaques)
+                   .HasColumnName("quantidade_maxima_destaques");
+
+            builder.Property(config => config.BannerUrl)
+                   .HasColumnName("banner_url");
+
+            builder.Property(config => config.LogoUrl)
+                   .HasColumnName("logo_url");
+
+            builder.Property(config => config.IconeUrl)
+                   .HasColumnName("icone_url");
+
+            builder.Property(config => config.CriadoEm)
+                   .HasColumnName("criado_em");
+
+            builder.Property(config => config.AtualizadoEm)
+                   .HasColumnName("atualizado_em");
+        }
     }
 }
--- backend/ConfeitariaWeb/ConfeitariaWeb/Data/Configurations/ProdutoConfiguration.cs
+++ backend/ConfeitariaWeb/ConfeitariaWeb/Data/Configurations/ProdutoConfiguration.cs
@@ -1,6 +1,57 @@
-﻿namespace ConfeitariaWeb.Data.Configurations
+﻿using ConfeitariaWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConfeitariaWeb.Data.Configurations
 {
-    public class ProdutoConfiguration
+    public class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
     {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("produtos");
+
+            builder.HasKey(p => p.IdProduto);
+
+            builder.Property(p => p.IdProduto)
+                   .HasColumnName("id_produto");
+
+            builder.Property(p => p.NomeProduto)
+                   .HasColumnName("nome_produto");
+
+            builder.Property(p => p.DescricaoProduto)
+                   .HasColumnName("descricao_produto");
+
+            builder.Property(p => p.ImagemUrl)
+                   .HasColumnName("imagem_url");
+
+            builder.Property(p => p.Destaque)
+                   .HasColumnName("destaque");
+
+            builder.Property(p => p.Ativo)
+                   .HasColumnName("ativo");
+
+            builder.Property(p => p.PrazoEntrega)
+                   .HasColumnName("prazo_entrega");
+
+            builder.Property(p => p.Slug)
+                   .HasColumnName("slug");
+
+            builder.Property(p => p.Preco)
+                   .HasColumnName("preco");
+
+            builder.Property(p => p.CriadoEm)
+                   .HasColumnName("criado_em");
+
+            builder.Property(p => p.AtualizadoEm)
+                   .HasColumnName("atualizado_em");
+
+            builder.Property(p => p.CategoriaId)
+               .HasColumnName("categoria_id");
+
+            builder.HasOne(p => p.Categoria)
+                   .WithMany(c => c.Produtos)
+                   .HasForeignKey(p => p.CategoriaId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
