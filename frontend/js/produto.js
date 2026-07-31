6186238f5da0444add570c7cb4859a6338c6602a
LucianoR8 <luciribsantos@gmail.com>
2026-07-30 22:33:11 -0300

    Requisição de informações de produtos chegando via HTTP
    A tela de produtos está recebendo os produtos cadastrados no banco de dados, com os campos preenchidos e a imagem disponível.
    
    Próximo passo: organizar as informações dos produtos em formato de cards no painel administrativo.

---
+++ frontend/js/produto.js
@@ -0,0 +1,42 @@
+import { verificarAutenticacao } from "./auth.js";
+import { get } from "./api.js";
+
+verificarAutenticacao();
+
+const listaProdutos = document.getElementById("listaProdutos");
+
+async function carregarProdutos(){
+
+    const produtos = await get("/produtos");
+
+    listaProdutos.innerHTML = "";
+
+    for(const produto of produtos){
+        const coluna = document.createElement("div");
+        
+        coluna.className = "produto-coluna";
+
+        coluna.innerHTML = `
+            <article class = "produto-card">
+            <img
+                src = "${produto.imagemUrl}"
+                alt = "${produto.nomeProduto}"
+                class = "produto-imagem">
+
+                <h3 class = "produto-nome"> ${produto.nomeProduto}</h3>
+                <p class = "produto-preco">R$ ${produto.preco}</p>
+            </article>
+            `;
+
+        listaProdutos.appendChild(coluna);
+
+        
+    }
+
+    
+
+    console.log(produtos);
+}
+
+carregarProdutos();
+
